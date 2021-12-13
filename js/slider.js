5a06d370e82b392a0ab59952007e01d5efd43e6c
Keyla_Triana <keyla.triana@nivelics.co>
2021-12-13 15:12:23 -0500

    slider1

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick(
+    {
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots:true,
+        prevArrow:'<i class="icon-chevron-left left-arrow"></i>',
+        nextArrow:'<i class="icon-chevron-right right-arrow"></i>',
+
+    }
+);
