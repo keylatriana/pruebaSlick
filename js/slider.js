b2cbafeb7da7d372bc193fc81e8756f8e3ed7f1a
Keyla_Triana <keyla.triana@nivelics.co>
2021-12-14 15:05:45 -0500

    ajustes del slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){ 
 $('.slider').slick(
     {
         autoplay: true,
@@ -8,12 +9,38 @@ $('.slider').slick(
 
     }
 );
-$('.card-slider').slick(
-    {
+//Contador del Slider- Card
+var $slider = $('.card-slider');
+
+if ($slider.length) {
+  var currentSlide;
+  var slidesCount;
+  var sliderCounter = document.createElement('div');
+  sliderCounter.classList.add('slider-card__counter');
+  
+  var updateSliderCounter = function(slick, currentIndex) {
+    currentSlide = slick.slickCurrentSlide() + 1;
+    slidesCount = slick.slideCount;
+    $(sliderCounter).text(currentSlide + '/' +slidesCount)
+  };
+
+  $slider.on('init', function(event, slick) {
+    $slider.append(sliderCounter);
+    updateSliderCounter(slick);
+  });
+
+  $slider.on('afterChange', function(event, slick, currentSlide) {
+    updateSliderCounter(slick, currentSlide);
+  });
+  
+
+  $slider.slick(
+      {
         dots:true,
         infinite:false,
         prevArrow:'<i class="icon-chevron-left left-arrow"></i>',
         nextArrow:'<i class="icon-chevron-right right-arrow"></i>',
-
-    }
-);
+      }
+  );
+}
+});
